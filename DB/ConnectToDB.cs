fd60de1ad4ad0098bfec2afd246cb98291d6a65b
MrMean <morteza_hoseyni81@yahoo.com>
2022-04-28 20:44:49 +0430

    DB Connection

---
+++ DB/ConnectToDB.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SQLite;
+
+namespace libraryManagement.DB
+{
+    class ConnectToDB
+    {
+        static SQLiteConnection CreateConnection()
+        {
+            SQLiteConnection sqlite_conn;
+            // Create a new database connection:
+            sqlite_conn = new SQLiteConnection("Data Source=libraryDatabase.db; Version = 3; New = True; Compress = True;");
+            // Open the connection:
+            sqlite_conn.Open();
+            return sqlite_conn;
+        }
+    }
+}
